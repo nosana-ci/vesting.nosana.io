8d5916717b241d772de106dde2fd47e78d92fbab
Laurens Verspeek <l.verspeek@gmail.com>
2022-01-19 18:20:31 +0100

    reload link

--- pages/index.vue
+++ pages/index.vue
@@ -17,14 +17,14 @@
                <div class="button is-medium is-accent" @click="$sol.loginModal = true">
               <strong>Connect Wallet</strong>
             </div>
-            <h3 class="subtitle mt-4">
+            <h3 class="subtitle my-3">
               - OR -
             </h3>
                </div>
         <form @submit.prevent="retrieveVestingContracts(address)" v-if="!vestings">
           <input v-model="address" type="text" class="input is-medium is-primary is-transparent" required placeholder="SOL address" />
           <button :disabled="loading" :class="{'is-disabled': loading}" class="button is-medium is-accent is-fullwidth mt-5" type="submit">
-            <strong>Submit</strong>
+            <strong>Find Vesting Contracts</strong>
           </button>
         </form>
         <div v-for="vesting in vestings" :key="vesting.pubkey.toString()">
@@ -33,8 +33,11 @@
         <div v-if="loading">
           Loading..
         </div>
-        <div v-if="vestings && !vestings.length">
+        <div v-else-if="vestings && !vestings.length">
           No vesting contracts found
+        </div>
+        <div v-else-if="vestings" class="mt-6">
+          <a @click.prevent="reload" class="is-size-6  has-text-danger ">Use different address</a>
         </div>
       </div>
     </div>
@@ -73,6 +76,9 @@ export default {
   },
 
   methods: {
+    reload() {
+      window.location.reload(true)
+    },
     async retrieveVestingContracts(publicKey) {
       this.loading = true
       try {
